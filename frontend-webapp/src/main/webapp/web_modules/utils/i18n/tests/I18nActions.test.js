af603ec7c6b1dee1443936d66a8564e2f9196931
lmieulet <leo.mieulet@c-s.fr>
2016-11-04 15:09:28 +0100

    try to fix test timeout

--- frontend-webapp/src/main/webapp/web_modules/utils/i18n/tests/I18nActions.test.js
+++ frontend-webapp/src/main/webapp/web_modules/utils/i18n/tests/I18nActions.test.js
@@ -11,7 +11,6 @@ const mockStore = configureStore(middlewares)
 describe('[COMMON] Testing i18n actions', () => {
   // Test dégradé dans le cas ou le serveur renvoie un erreur
   it('Test actions creation for internationalization language change', () => {
-    this.timeout(10000)
     const store = mockStore({
       common: {
         i18n: {
