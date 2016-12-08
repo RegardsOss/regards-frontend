74ab67f14445999246f97d087516d7c9cdaf0c62
xbrochar <xavier-alexandre.brochard@c-s.fr>
2016-12-08 20:23:53 +0100

    Completed a Symbol description

--- frontend-webapp/src/main/webapp/web_modules/modules/admin-database-management/src/components/DatabaseConnectionTesterWithFlatButton.jsx
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-database-management/src/components/DatabaseConnectionTesterWithFlatButton.jsx
@@ -15,7 +15,7 @@ import { FormattedMessage } from 'react-intl'
 const NOT_TESTED = Symbol('The connection has not been tested yet')
 const PENDING = Symbol('The connection is being established')
 const SUCCESS = Symbol('The connection has successfuly been established')
-const WARNING = Symbol('TODO')
+const WARNING = Symbol('The connection could be established but errors occured')
 const ERROR = Symbol('The connection could not be established')
 
 class DatabaseConnectionTesterWithFlatButton extends React.Component {
