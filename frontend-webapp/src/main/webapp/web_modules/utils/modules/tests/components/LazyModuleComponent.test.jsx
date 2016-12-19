3c60c08771bf98edf62bcd539bb8c313559680d7
lmieulet <leo.mieulet@c-s.fr>
2016-12-19 11:56:00 +0100

    add error on basic actions

--- frontend-webapp/src/main/webapp/web_modules/utils/modules/tests/components/LazyModuleComponent.test.jsx
+++ frontend-webapp/src/main/webapp/web_modules/utils/modules/tests/components/LazyModuleComponent.test.jsx
@@ -3,8 +3,8 @@
  **/
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
-import { LazyModuleComponent } from '@regardsoss/modules'
 import { moduleContainer } from '@regardsoss/authentication'
+import LazyModuleComponent from '../../src/components/LazyModuleComponent'
 
 describe('[MODULES] Testing LazyModuleComponent', () => {
   it('Should render correctly an application layout with ApplicationLayout', () => {
