eb36a14b450211f5d83a05b9d7bbe8e7dd42c109
lmieulet <leo.mieulet@c-s.fr>
2016-12-20 18:00:53 +0100

    Remove sonar reporter and fix LazyModuleComponent test timeout

--- frontend-webapp/src/main/webapp/web_modules/utils/modules/tests/components/LazyModuleComponent.test.jsx
+++ frontend-webapp/src/main/webapp/web_modules/utils/modules/tests/components/LazyModuleComponent.test.jsx
@@ -7,6 +7,7 @@ import { moduleContainer } from '@regardsoss/authentication'
 import LazyModuleComponent from '../../src/components/LazyModuleComponent'
 
 describe('[MODULES] Testing LazyModuleComponent', () => {
+  // This test can last ~6s so we override the timeout duration
   it('Should render correctly an application layout with ApplicationLayout', () => {
     const context = {
 
@@ -17,5 +18,5 @@ describe('[MODULES] Testing LazyModuleComponent', () => {
     const wrapper = shallow(<LazyModuleComponent appName={'testApp'} module={module} />, { context })
 
     expect(wrapper.find(moduleContainer)).to.have.length(1)
-  })
+  }).timeout(20000)
 })
