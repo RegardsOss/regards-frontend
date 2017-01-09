e3a7f8a3fb54be30f8346e7952a873406a6898dd
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-01-09 19:29:39 +0100

    [Ms Mgmt] Implemented the board.

---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/src/containers/MicroserviceBoardContainer.jsx
@@ -0,0 +1,33 @@
+/**
+ * LICENSE_PLACEHOLDER
+ **/
+import { I18nProvider, i18nContextType } from '@regardsoss/i18n'
+import MicroserviceBoardComponent from '../components/MicroserviceBoardComponent'
+
+/**
+ * Module container connecting {@link MicroserviceBoardComponent} to redux in order to display the list of microservices.
+ *
+ * @author Xavier-Alexandre Brochard
+ */
+class MicroserviceBoardContainer extends React.Component {
+
+  static propTypes = {
+    params: React.PropTypes.shape({
+      project: React.PropTypes.string,
+    }),
+  }
+
+  static contextTypes = {
+    ...i18nContextType,
+  }
+
+  render() {
+    return (
+      <I18nProvider messageDir="modules/admin-microservice-management/src/i18n">
+        <MicroserviceBoardComponent project={this.props.params.project} />
+      </I18nProvider>
+    )
+  }
+}
+
+export default MicroserviceBoardContainer
--- frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/src/i18n/messages.fr.i18n.js
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/src/i18n/messages.fr.i18n.js
@@ -1,7 +1,15 @@
 import { Locales } from '@regardsoss/form-utils'
 
 const messages = Object.assign({
+  'microservice-management.rs-access.description': 'Configuration du microservice de gestion de l\'interface utilisateur',
+  'microservice-management.rs-admin.description': 'Configuration du microservice d\'administration du système',
+  'microservice-management.rs-cloud.description': 'Configuration du microservice de configuration',
+  'microservice-management.rs-dam.description': 'Configuration du microservice de catalogage des données',
+  'microservice-management.rs-gateway.description': 'Configuration de la passerelle',
 
+  'microservice-management.configuration.tooltip': 'Configurer',
+  'microservice-management.plugins.tooltip': 'Plugins',
+  'microservice-management.maintenance.tooltip': 'Mode maintenance',
 }, Locales.fr)
 
 export default messages
