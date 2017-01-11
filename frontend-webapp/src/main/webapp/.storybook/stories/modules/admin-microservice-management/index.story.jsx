f1c3c8d8dfd2da79b03476f9ca489952ff4ef438
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-01-11 09:49:29 +0100

    [Board] WIP: improving board as a card.

---
+++ frontend-webapp/src/main/webapp/.storybook/stories/modules/admin-microservice-management/index.story.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import { withKnobs } from '@kadira/storybook-addon-knobs'
+import { StoreDecorator, addLocaleAndThemeSelectors, ThemeAndLocaleDecorator } from '../../utils/decorators'
+import MicroservicePluginTypesComponent from '@regardsoss/admin-microservice-management/src/components/MicroservicePluginTypesComponent'
+import MicroservicePluginsComponent from '@regardsoss/admin-microservice-management/src/components/MicroservicePluginsComponent'
+import SexyMicroserviceBoardComponent from '@regardsoss/admin-microservice-management/src/components/SexyMicroserviceBoardComponent'
+
+storiesOf('Admin - Microservice management', module)
+  .addDecorator(withKnobs)
+  .addDecorator(StoreDecorator)
+  .add('Microservice plugin types', () => {
+      const themeName = addLocaleAndThemeSelectors()
+      return (
+        <ThemeAndLocaleDecorator theme={themeName} messageDir="modules/admin-microservice-management/src/i18n">
+          <MicroservicePluginTypesComponent microserviceName='rs-gateway'/>
+        </ThemeAndLocaleDecorator>
+      )
+    })
+    .add('Microservice plugins', () => {
+      const themeName = addLocaleAndThemeSelectors()
+      return (
+        <ThemeAndLocaleDecorator theme={themeName} messageDir="modules/admin-microservice-management/src/i18n">
+          <MicroservicePluginsComponent microserviceName='rs-gateway'/>
+        </ThemeAndLocaleDecorator>
+      )
+    })
+    .add('Microservice plugin configuration edit', () => {
+      const themeName = addLocaleAndThemeSelectors()
+      return (
+        <ThemeAndLocaleDecorator theme={themeName} messageDir="modules/admin-microservice-management/src/i18n">
+          <MicroservicePluginConfigurationEditComponent microserviceName='rs-gateway'/>
+        </ThemeAndLocaleDecorator>
+      )
+    })
+    .add('New board component', () => {
+      const themeName = addLocaleAndThemeSelectors()
+      return (
+        <ThemeAndLocaleDecorator theme={themeName} messageDir="modules/admin-microservice-management/src/i18n">
+          <SexyMicroserviceBoardComponent microserviceName='rs-gateway'/>
+        </ThemeAndLocaleDecorator>
+      )
+    })
