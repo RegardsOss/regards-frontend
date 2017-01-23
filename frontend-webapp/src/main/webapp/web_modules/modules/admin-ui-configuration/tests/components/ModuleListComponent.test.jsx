d51d2e126e67f3f8e33e2bc69ca283bc631f3ff2
sbinda <sebastien.binda@c-s.fr>
2017-01-23 18:14:55 +0100

    Add unit tests.

--- frontend-webapp/src/main/webapp/web_modules/modules/admin-ui-configuration/tests/components/ModuleListComponent.test.jsx
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-ui-configuration/tests/components/ModuleListComponent.test.jsx
@@ -4,46 +4,106 @@
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import { TableBody, TableRow } from 'material-ui/Table'
+import sinon from 'sinon'
+import Edit from 'material-ui/svg-icons/editor/mode-edit'
+import Delete from 'material-ui/svg-icons/action/delete'
+import IconButton from 'material-ui/IconButton'
 import ModuleListComponent from '../../src/components/ModuleListComponent'
+
+const options = {
+  context: {
+    muiTheme: {
+      palette: {
+        primary1Color: 'White',
+        accent1Color: 'White',
+        pickerHeaderColor: 'White',
+      },
+    },
+    intl: {
+      formatMessage: opt => opt.id,
+      formatTime: () => {}
+    },
+  },
+}
+
+const testModules = {
+  0: {
+    content: {
+      id: 0,
+      name: 'module',
+      description: 'First Module',
+      active: true,
+      applicationId: 'user',
+      container: 'content',
+      conf: {},
+    },
+  },
+  1: {
+    content: {
+      id: 1,
+      name: 'module',
+      description: 'Second Module',
+      active: true,
+      applicationId: 'user',
+      container: 'content',
+      conf: {},
+    },
+  },
+  2: {
+    content: {
+      id: 2,
+      name: 'module',
+      description: 'Third Module',
+      active: false,
+      applicationId: 'user',
+      container: 'content',
+      conf: {},
+    },
+  }
+}
 
 describe('[ADMIN UI-CONFIGURATION] Testing Modules list component', () => {
   it('Should render correctly a list of availables modules', () => {
-    const options = {
-      context: {
-        muiTheme: {
-          palette: {
-            primary1Color: 'White',
-            accent1Color: 'White',
-            pickerHeaderColor: 'White',
-          },
-        },
-        intl: {
-          formatMessage: opt => opt.id,
-        },
-      },
-    }
+
     const props = {
-      modules: [{
-        content: {
-          id: 1,
-          name: 'module',
-          description: 'First Module',
-          active: true,
-          applicationId: 'user',
-          container: 'content',
-          conf: {},
-        },
-      }],
+      modules: testModules,
+      onActivation: () => {},
       onCreate: () => {},
       onEdit: () => {},
       onDelete: () => {},
-      onCreation: () => {},
     }
 
     const wrapper = shallow(
       <ModuleListComponent {...props} />
     , options)
 
-    expect(wrapper.find(TableBody).find(TableRow)).to.have.length(1)
+    expect(wrapper.find(TableBody).find(TableRow)).to.have.length(3)
+  })
+
+  it('Check actions on ModuleListComponent', () => {
+
+    const onEditCallback = sinon.spy()
+    const onDeleteCallback = sinon.spy()
+
+    const props = {
+      modules: testModules,
+      onActivation: () => {},
+      onCreate: ()=> {},
+      onEdit: onEditCallback,
+      onDelete: onDeleteCallback,
+    }
+
+    const wrapper = shallow(
+      <ModuleListComponent {...props} />
+      , options)
+
+
+    const buttons = wrapper.find(TableBody).find(TableRow).find(IconButton)
+    expect(buttons).to.have.length(6)
+
+    const editButton = buttons.first()
+    editButton.simulate('touchTap')
+    expect(onEditCallback.calledOnce).to.equal(true)
+
   })
 })
