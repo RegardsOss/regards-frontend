272f0365f1dd922594b816dcb171964592acfb7a
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-01-31 15:40:42 +0100

    [Plugin Parameters] Case where param is a plugin.
    Done: Populated mock & added the select field.
    Todo: Plug the select field to the list of available configurations.

--- frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/src/components/PluginParameterComponent.jsx
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/src/components/PluginParameterComponent.jsx
@@ -1,11 +1,18 @@
 /**
  * LICENSE_PLACEHOLDER
  **/
+import { chain } from 'lodash'
 import { ListItem } from 'material-ui/List'
-import { Toggle } from 'redux-form-material-ui'
+import MenuItem from 'material-ui/MenuItem'
+import Subheader from 'material-ui/Subheader'
+import Divider from 'material-ui/Divider'
+import { Toggle, SelectField } from 'redux-form-material-ui'
 import { PluginParameter, PluginParameterType } from '@regardsoss/model'
 import { RenderTextField, Field } from '@regardsoss/form-utils'
 import moduleStyles from '../styles/styles'
+
+// validation functions
+const required = value => value == null ? 'Required' : undefined
 
 /**
  * Renders a field in view or edit mode based on a pluginParameter and it's type.
@@ -37,57 +44,92 @@ class PluginParameterComponent extends React.Component {
     const styles = moduleStyles(this.context.muiTheme)
     const { pluginParameter, pluginParameterType, mode } = this.props
     const { name, value } = pluginParameter
+    const pluginConfigurationList = []
 
     switch (mode) {
       case 'view':
         return <ListItem>{name}: {value.toString()}</ListItem>
       case 'edit':
-        switch (pluginParameterType.type) {
-          case 'java.lang.String':
-          case 'java.lang.Character':
-            return (
-              <Field
-                name={name}
-                fullWidth
-                component={RenderTextField}
-                type={'text'}
-                label={name}
-              />
-            )
-          case 'java.lang.Byte':
-          case 'java.lang.Integer':
-          case 'java.lang.Long':
-          case 'java.lang.Float':
-          case 'java.lang.Double':
-          case 'java.lang.Short':
-            return (
-              <Field
-                name={name}
-                fullWidth
-                component={RenderTextField}
-                type={'number'}
-                label={name}
-              />
-            )
-          case 'java.lang.Boolean':
-            return (
-              <Field
-                name={name}
-                component={Toggle}
-                type={'boolean'}
-                style={styles.pluginConfiguration.form.toggle}
-                label={name}
-              />
-            )
+        switch (pluginParameterType.paramType) {
+          case 'PRIMITIVE':
+            switch (pluginParameterType.type) {
+              case 'java.lang.String':
+              case 'java.lang.Character':
+                return (
+                  <Field
+                    name={name}
+                    fullWidth
+                    component={RenderTextField}
+                    type={'text'}
+                    label={name}
+                  />
+                )
+              case 'java.lang.Byte':
+              case 'java.lang.Integer':
+              case 'java.lang.Long':
+              case 'java.lang.Float':
+              case 'java.lang.Double':
+              case 'java.lang.Short':
+                return (
+                  <Field
+                    name={name}
+                    fullWidth
+                    component={RenderTextField}
+                    type={'number'}
+                    label={name}
+                  />
+                )
+              case 'java.lang.Boolean':
+                return (
+                  <Field
+                    name={name}
+                    component={Toggle}
+                    type={'boolean'}
+                    style={styles.pluginConfiguration.form.toggle}
+                    label={name}
+                  />
+                )
+              default:
+                return (
+                  <Field
+                    name={name}
+                    fullWidth
+                    component={RenderTextField}
+                    type={'text'}
+                    label={name}
+                  />
+                )
+            }
+          case 'PLUGIN':
           default:
             return (
               <Field
                 name={name}
+                component={SelectField}
+                hintText={`${name} - ${pluginParameterType.type}`}
+                floatingLabelText={`${name} - ${pluginParameterType.type}`}
+                validate={required}
                 fullWidth
-                component={RenderTextField}
-                type={'text'}
-                label={name}
-              />
+                autoWidth
+              >
+                <Subheader>Active</Subheader>
+                {chain(pluginConfigurationList)
+                  .filter(pluginConfiguration => pluginConfiguration.active)
+                  .sort(pluginConfiguration => pluginConfiguration.priorityOrder)
+                  .map(pluginConfiguration => (
+                    <MenuItem value={pluginConfiguration.id} primaryText={pluginConfiguration.label}/>
+                  ))
+                  .value()}
+                <Divider />
+                <Subheader>Inaactive</Subheader>
+                {chain(pluginConfigurationList)
+                  .filter(pluginConfiguration => !pluginConfiguration.active)
+                  .sort(pluginConfiguration => pluginConfiguration.priorityOrder)
+                  .map(pluginConfiguration => (
+                    <MenuItem value={pluginConfiguration.id} primaryText={pluginConfiguration.label}/>
+                  ))
+                  .value()}
+              </Field>
             )
         }
       default:
