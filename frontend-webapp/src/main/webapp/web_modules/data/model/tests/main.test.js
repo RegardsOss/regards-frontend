0c90b826021bacf8896a2cf836eca7460cf3e5dc
lmieulet <leo.mieulet@c-s.fr>
2017-02-01 16:08:21 +0100

    Add coverage to data/model

---
+++ frontend-webapp/src/main/webapp/web_modules/data/model/tests/main.test.js
@@ -0,0 +1,57 @@
+import { assert } from 'chai'
+import Models from '../src/main'
+
+describe('[MODEL] Testing all models (aka shape in the react world)', () => {
+  it('should exists', () => {
+    assert.isDefined(Models)
+  })
+  describe('should define shapes of rs-', () => {
+    const shallExist = model => function () {
+      assert.isDefined(model)
+      assert.isFunction(model, model)
+    }
+    describe('access', () => {
+      it(`check Plugin`, shallExist(Models.Plugin))
+      it(`check PluginDefinition`, shallExist(Models.PluginDefinition))
+      it(`check PluginInfo`, shallExist(Models.PluginInfo))
+      it(`check PluginConf`, shallExist(Models.PluginConf))
+      it(`check AccessProject`, shallExist(Models.AccessProject))
+      it(`check Module`, shallExist(Models.Module))
+      it(`check Layout`, shallExist(Models.Layout))
+      it(`check LayoutContent`, shallExist(Models.LayoutContent))
+    })
+    describe('admin', () => {
+      it(`check ProjectUser`, shallExist(Models.ProjectUser))
+      it(`check Role`, shallExist(Models.Role))
+      it(`check Project`, shallExist(Models.Project))
+      it(`check Account`, shallExist(Models.Account))
+      it(`check Resource`, shallExist(Models.Resource))
+      it(`check ResourceList`, shallExist(Models.ResourceList))
+      it(`check Endpoint`, shallExist(Models.Endpoint))
+
+      it(`check PluginMetaData`, shallExist(Models.PluginMetaData))
+      it(`check PluginMetaDataList`, shallExist(Models.PluginMetaDataList))
+      it(`check PluginConfiguration`, shallExist(Models.PluginConfiguration))
+      it(`check PluginConfigurationList`, shallExist(Models.PluginConfigurationList))
+      it(`check PluginParameter`, shallExist(Models.PluginParameter))
+      it(`check PluginParameterist`, shallExist(Models.PluginParameterist))
+      it(`check PluginDynamicValue`, shallExist(Models.PluginDynamicValue))
+      it(`check PluginDynamicValueList`, shallExist(Models.PluginDynamicValueList))
+    })
+    describe('archival-storage', () => {
+      it(`check AIPStatus`, shallExist(Models.AIPStatus))
+      it(`check StoragePlugin`, shallExist(Models.StoragePlugin))
+      it(`check StoragePluginShape`, shallExist(Models.StoragePluginShape))
+    })
+    describe('catalog', () => {
+      it(`check Entity`, shallExist(Models.Entity))
+    })
+    describe('dam', () => {
+      it(`check AttributeModel`, shallExist(Models.AttributeModel))
+      it(`check Fragment`, shallExist(Models.Fragment))
+      it(`check Model`, shallExist(Models.Model))
+      it(`check ModelAttribute`, shallExist(Models.ModelAttribute))
+      it(`check Collection`, shallExist(Models.Collection))
+    })
+  })
+})
