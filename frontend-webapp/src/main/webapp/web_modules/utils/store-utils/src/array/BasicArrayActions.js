660c4ffaa1f08465ee7612de5fdcfc131598e2db
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-02-01 10:35:12 +0100

    Refactored Basic Actions.
    And corrected a bug on handleRequestParams.

--- frontend-webapp/src/main/webapp/web_modules/utils/store-utils/src/array/BasicArrayActions.js
+++ frontend-webapp/src/main/webapp/web_modules/utils/store-utils/src/array/BasicArrayActions.js
@@ -1,47 +1,25 @@
 /**
  * LICENSE_PLACEHOLDER
  **/
-
-/**
- * @author Léo Mieulet
- */
 import { map, replace } from 'lodash'
+import BasicActions from '../BasicActions'
 import ErrorHandler from '../ErrorHandler'
 
 const { CALL_API, getJSON } = require('redux-api-middleware')
 /**
  *  Provide actions to retrieve an array of value
+ *
  *  @Return dispatcheable redux actions
+ *  @author Léo Mieulet
  */
-class BasicArrayActions {
-
-  constructor(options) {
-    this.entityEndpoint = options.entityEndpoint
-    this.ENTITY_LIST_REQUEST = `${options.namespace}/LIST_REQUEST`
-    this.ENTITY_LIST_SUCCESS = `${options.namespace}/LIST_SUCCESS`
-    this.ENTITY_LIST_FAILURE = `${options.namespace}/LIST_FAILURE`
-
-    this.FLUSH = `${options.namespace}/FLUSH`
-
-    this.errorHandler = new ErrorHandler()
-  }
+class BasicArrayActions extends BasicActions {
 
   /**
-   * Replace parameterized value in the current configured endpoint
-   * @param entityEndpoint endpoint entity
-   * @param params parameters to replace in the endpoint entity
-   * @returns {*}
+   * TODO Comment
+   *
+   * @param params TODO Provide params expected format
+   * @returns {{}}
    */
-  handleRequestParameters = (entityEndpoint, params) => {
-    let endpoint = entityEndpoint
-    if (params) {
-      map(params, (param, id) => {
-        endpoint = replace(endpoint, `%${id}`, param)
-      })
-    }
-    return endpoint
-  }
-
   fetchEntityList(params) {
     const endpoint = this.handleRequestParameters(this.entityEndpoint, params)
     return {
@@ -60,15 +38,7 @@ class BasicArrayActions {
     }
   }
 
-  /**
-   * Remove all existing entries
-   * @returns {{type: string}}
-   */
-  flush() {
-    return {
-      type: this.FLUSH,
-    }
-  }
+
 }
 
 
