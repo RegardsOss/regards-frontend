68a49016b1a8c372615dea51e664b262298a2f56
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-02-07 17:53:02 +0100

    [Plugin Criteria] Implemented the composed logic agregator.

--- frontend-webapp/src/main/webapp/plugins/criterion/two-numerical/src/components/TwoNumericalCriteriaComponent.jsx
+++ frontend-webapp/src/main/webapp/plugins/criterion/two-numerical/src/components/TwoNumericalCriteriaComponent.jsx
@@ -1,10 +1,20 @@
 /**
  * LICENSE_PLACEHOLDER
  **/
-import { mapValues } from 'lodash'
-import Paper from 'material-ui/Paper'
+import { mapValues, chain } from 'lodash'
+import TwoNumericalCriteriaSimpleComponent from './TwoNumericalCriteriaSimpleComponent'
+import TwoNumericalCriteriaComposedComponent from './TwoNumericalCriteriaComposedComponent'
 
 /**
+ * Search form criteria plugin allowing the user to configure the numerical value of two different attributes with comparators.
+ *
+ * Below is an example of the simple layout for two different attributes :
+ * attribute1 < value1 and attribute2 != value2
+ *
+ * TODO + TBC
+ * Now if the two passed attributes are the same, we switch to as composed layout:
+ * value1 <= attribute <= value2
+ *
  * @author Xavier-Alexandre Brochard
  */
 export class TwoNumericalCriteriaComponent extends React.Component {
@@ -32,7 +42,8 @@ export class TwoNumericalCriteriaComponent extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isComposed: false, // TODO
+      // Switch to composed mode if both passed attributes are the same
+      isComposed: chain(props.attributes).map('name').uniq().value().length === 1,
       criteria: mapValues(props.attributes, el => ({
         value: undefined,
         comparator: 'EQ',
@@ -40,79 +51,9 @@ export class TwoNumericalCriteriaComponent extends React.Component {
     }
   }
 
-  // /**
-  //  * Callback function that is fired when the textfield's value changes.
-  //  *
-  //  * @param {Object} event Change event targeting the text field.
-  //  * @param {String} newValue The new value of the text field.
-  //  */
-  // handleChangeValue = (event, newValue) => {
-  //   const { attributes, pluginInstanceId, onChange } = this.props
-  //   const { comparator } = this.state
-  //   this.setState({
-  //     value: this.parse(newValue),
-  //   })
-  //   onChange({
-  //     attribute: attributes.searchField,
-  //     comparator,
-  //     value: newValue,
-  //   }, pluginInstanceId)
-  // }
-
-  // handleChangeComparator = (comparator) => {
-  //   this.setState({
-  //     comparator,
-  //   })
-  // }
-
-  // /**
-  //  * Parses the value given from the field input component.
-  //  *
-  //  * @param {String} value
-  //  */
-  // parse = value => parseFloat(value)
-
-  // /**
-  //  * Formats the value before displaying in the field input component.
-  //  *
-  //  * @param {String} value
-  //  */
-  // format = value => value
-
   render() {
-    const attributeLabel = this.props.attributes && this.props.attributes.searchField && this.props.attributes.searchField.name
-
-    return (
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'baseline',
-      }}>
-        <Paper style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: '0px 16px',
-          maxWidth: 400,
-        }}>
-          /*
-           <span>{attributeLabel}</span>
-           <NumericalComparatorComponent onChange={this.handleChangeComparator}/>
-           <TextField
-           id="search"
-           type="number"
-           floatingLabelText={'Nombre...'} // TODO
-           value={this.format(this.state.value)}
-           onChange={this.handleChangeValue}
-           style={{
-           top: -10,
-           width: '33%',
-           }}
-           />
-           */
-        </Paper>
-      </div>
-    )
+    const { isComposed } = this.state
+    return isComposed ? <TwoNumericalCriteriaComposedComponent {...this.props} /> : <TwoNumericalCriteriaSimpleComponent {...this.props} />
   }
 }
 
