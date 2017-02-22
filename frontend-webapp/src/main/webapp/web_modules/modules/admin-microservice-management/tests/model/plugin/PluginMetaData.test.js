7bbb60b53a471456fd7475e344b17ac1a7a1ec74
Maxime Bouveron <maxime.bouveron@c-s.fr>
2017-02-22 17:49:51 +0100

    Added tests for admin microservice management plugins

---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/tests/model/plugin/PluginMetaData.test.js
@@ -0,0 +1,29 @@
+/**
+ * LICENSE_PLACEHOLDER
+ **/
+import { ReduxEntityTester } from '@regardsoss/tests-helpers'
+import { PluginMetaData } from '@regardsoss/model'
+import PluginMetaDataActions from '../../../src/model/plugin/PluginMetaDataActions'
+import PluginMetaDataReducers from '../../../src/model/plugin/PluginMetaDataReducers'
+import PluginMetaDataSelectors from '../../../src/model/plugin/PluginMetaDataSelectors'
+import PluginMetaDataNetworkDump from './dump/PluginMetaDataNetworkDump'
+
+const backendServerResultList = PluginMetaDataNetworkDump
+const options = {
+  urlParams: { microserviceName: 'rs-dam' },
+}
+
+const entityTester = new ReduxEntityTester(PluginMetaDataActions, PluginMetaDataReducers, PluginMetaDataSelectors, React.PropTypes.objectOf(PluginMetaData).isRequired, backendServerResultList, options)
+
+describe('[ADMIN MICROSERVICE MANAGEMENT] Testing model PluginMetaData', () => {
+  before(() => {
+    entityTester.beforeAll()
+  })
+  after(() => {
+    entityTester.afterAll()
+  })
+  it('should retrieve the list of items, reduce it, and store it on the store.', (done) => {
+    entityTester.runTests(done)
+  })
+})
+
---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-microservice-management/tests/model/plugin/PluginType.test.js
@@ -0,0 +1,28 @@
+/**
+ * LICENSE_PLACEHOLDER
+ **/
+
+import { ReduxEntityTester } from '@regardsoss/tests-helpers'
+import PluginTypeActions from '../../../src/model/plugin/PluginTypeActions'
+import PluginTypeReducers from '../../../src/model/plugin/PluginTypeReducers'
+import PluginTypeSelectors from '../../../src/model/plugin/PluginTypeSelectors'
+import PluginTypeNetworkDump from './dump/PluginTypeNetworkDump'
+
+const backendServerResultList = PluginTypeNetworkDump
+const options = {
+  urlParams: { microserviceName: 'rs-dam' },
+}
+
+const entityTester = new ReduxEntityTester(PluginTypeActions, PluginTypeReducers, PluginTypeSelectors, React.PropTypes.objectOf(React.PropTypes.arrayOf(React.PropTypes.string)).isRequired, backendServerResultList, options)
+
+describe('[ADMIN MICROSERVICE MANAGEMENT] Testing model PluginType', () => {
+  before(() => {
+    entityTester.beforeAll()
+  })
+  after(() => {
+    entityTester.afterAll()
+  })
+  it('should retrieve the list of items, reduce it, and store it on the store.', (done) => {
+    entityTester.runTests(done)
+  })
+})
