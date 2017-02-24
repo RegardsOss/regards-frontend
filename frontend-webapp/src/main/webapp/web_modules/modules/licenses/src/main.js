3e1a3a828e4a6942924243f8fc434ae2acebafbf
RaphaelMechali <raphael.mechali@gmail.com>
2017-02-24 14:48:43 +0100

    Initialized module
    Initialized module, but stopped here as license task is not ready (need more information)

---
+++ frontend-webapp/src/main/webapp/web_modules/modules/licenses/src/main.js
@@ -0,0 +1,14 @@
+/**
+ * LICENSE_PLACEHOLDER
+ **/
+
+
+/**
+ * Main file of module to expose public interface
+ **/
+export default {
+  // styles,
+  // moduleContainer,
+  // reducer,
+  messagesDir: 'modules/licenses/src/i18n',
+}
---
+++ frontend-webapp/src/main/webapp/web_modules/modules/licenses/src/reducer.js
@@ -0,0 +1,11 @@
+/**
+ * LICENSE_PLACEHOLDER
+ */
+
+
+const licenseReducer = {
+
+}
+
+export default licenseReducer
+
