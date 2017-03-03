4f4ba5c694cb4d8feacb287c55af36bc3445a7c3
RaphaelMechali <raphael.mechali@gmail.com>
2017-03-03 19:15:57 +0100

    Model tests added
    and existing tests correction

---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/tests/model/Role.test.js
@@ -0,0 +1,28 @@
+/**
+ * LICENSE_PLACEHOLDER
+ **/
+import { ReduxEntityTester } from '@regardsoss/tests-helpers'
+import { Role } from '@regardsoss/model'
+import RoleActions from '../../src/model/RoleActions'
+import { getRoleReducer } from '../../src/model/RoleReducers'
+import RoleSelectors from '../../src/model/RoleSelectors'
+import RoleNetworkDump from './dump/RoleNetworkDump'
+
+const backendServerResultList = RoleNetworkDump
+const options = {
+}
+
+const entityTester = new ReduxEntityTester(RoleActions, getRoleReducer, RoleSelectors, React.PropTypes.objectOf(Role).isRequired, backendServerResultList, options)
+
+describe('[ADMIN Role MANAGEMENT] Testing model Role', () => {
+  before(() => {
+    entityTester.beforeAll()
+  })
+  after(() => {
+    entityTester.afterAll()
+  })
+  it('should retrieve the list of items, reduce it, and store it on the store.', (done) => {
+    entityTester.runTests(done)
+  })
+})
+
---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/tests/model/dump/AccessGroupNetworkDump.js
@@ -0,0 +1,131 @@
+export default {
+  content: [
+    {
+      content: {
+        id: 1,
+        name: 'AG1',
+        users: [
+          {
+            email: 'florian.philippot@facholand.fr',
+          },
+          {
+            email: 'mon@adresse.em',
+          },
+        ],
+        accessRights: [
+          {
+            qualityFilter: {
+              maxScore: '10',
+              minScore: '0',
+              qualityLevel: 'ACCEPTED',
+            },
+            dataAccessRight: {
+              dataAccessLevel: 'INHERITED_ACCESS',
+            },
+            accessLevel: 'FULL_ACCESS',
+            dataSet: {
+              id: 1,
+              label: 'PremierDataSet',
+              type: 'DATASET',
+              model: {
+                id: 2,
+                name: 'Premier Modele',
+                descirption: 'Description du premier modele de jeux de données',
+                type: 'DATASET',
+              },
+            },
+            id: 1,
+          },
+          {
+            qualityFilter: {
+              maxScore: '10',
+              minScore: '0',
+              qualityLevel: 'ACCEPTED',
+            },
+            dataAccessRight: {
+              dataAccessLevel: 'INHERITED_ACCESS',
+            },
+            accessLevel: 'FULL_ACCESS',
+            dataSet: {
+              id: 4,
+              label: 'QuatriemDataSet',
+              type: 'DATASET',
+              model: {
+                id: 2,
+                name: 'Premier Modele',
+                descirption: 'Description du premier modele de jeux de données',
+                type: 'DATASET',
+              },
+            },
+            id: 2,
+          },
+          {
+            qualityFilter: {
+              maxScore: '10',
+              minScore: '0',
+              qualityLevel: 'ACCEPTED',
+            },
+            dataAccessRight: {
+              dataAccessLevel: 'INHERITED_ACCESS',
+            },
+            accessLevel: 'FULL_ACCESS',
+            dataSet: {
+              id: 3,
+              label: 'TroisiemeDataSet',
+              type: 'DATASET',
+              model: {
+                id: 2,
+                name: 'Premier Modele',
+                descirption: 'Description du premier modele de jeux de données',
+                type: 'DATASET',
+              },
+            },
+            id: 3,
+          },
+          {
+            qualityFilter: {
+              maxScore: '6',
+              minScore: '4',
+              qualityLevel: 'REJECTED',
+            },
+            dataAccessRight: {
+              dataAccessLevel: 'CUSTOM_ACCESS',
+              pluginConfiguration: 40,
+            },
+            accessLevel: 'NO_ACCESS',
+            dataSet: {
+              id: 5,
+              label: 'CinquiemeDataSet',
+              type: 'DATASET',
+              model: {
+                id: 2,
+                name: 'Premier Modeleee',
+                descirption: 'Description du premier modele de jeux de données',
+                type: 'DATASET',
+              },
+            },
+            id: 4,
+          },
+        ],
+        isPrivate: true,
+      },
+      links: [],
+    },
+    {
+      content: {
+        id: 2,
+        name: 'AG2',
+        users: [],
+        accessRights: [],
+        isPrivate: true,
+      },
+      links: [],
+    },
+  ],
+  metadata: {
+    number: 0,
+    size: 10000,
+    totalElements: 2,
+  },
+  links: [],
+}
