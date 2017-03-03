8c702863b091fcde2f1579ad8afc67e1fd468770
Maxime Bouveron <maxime.bouveron@c-s.fr>
2017-03-03 17:10:16 +0100

    Added dumps for projectuser

---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/tests/model/dump/GroupListNetworkDump.js
@@ -0,0 +1,16 @@
+export default {
+  AG1: {
+    content: {
+      id: 1,
+      name: 'AG1',
+      users: [{ email: 'test@test.fr' }, { email: 'mon@adresse.em' }],
+      accessRights: [],
+      isPrivate: true,
+    },
+    links: [],
+  },
+  AG2: {
+    content: { id: 2, name: 'AG2', users: [], accessRights: [], isPrivate: true },
+    links: [],
+  },
+}
---
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/tests/model/dump/ProjectUserNetworkDump.js
@@ -0,0 +1,15 @@
+export default {
+  content: {
+    id: 1,
+    email: 'mon@adresse.em',
+    lastUpdate: {
+      date: { year: '2017', month: '1', day: '9' },
+      time: { hour: '15', minute: '46', second: '12', nano: '453000000' },
+    },
+    status: 'WAITING_ACCESS',
+    metaData: [],
+    role: { name: 'REGISTERED_USER' },
+    permissions: [],
+  },
+  links: [],
+}
