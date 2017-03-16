0a15526e4b9035c637b7fdb57f36b82110bc3e8d
lmieulet <leo.mieulet@c-s.fr>
2017-03-16 19:03:04 +0100

    Fix add/remove user from a group

--- frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/src/containers/ProjectUserCreateContainer.jsx
+++ frontend-webapp/src/main/webapp/web_modules/modules/admin-user-projectuser-management/src/containers/ProjectUserCreateContainer.jsx
@@ -3,7 +3,7 @@ import { connect } from '@regardsoss/redux'
 import { I18nProvider } from '@regardsoss/i18n'
 import { FormLoadingComponent, FormEntityNotFoundComponent } from '@regardsoss/form-utils'
 import { ProjectUser, AccessGroup } from '@regardsoss/model'
-import { forEach } from 'lodash'
+import { every, some, chain, concat } from 'lodash'
 import root from 'window-or-global'
 import { AuthenticationRouteParameters } from '@regardsoss/authentication-manager'
 import RoleActions from '../model/RoleActions'
@@ -41,6 +41,7 @@ export class ProjectUserCreateContainer extends React.Component {
     fetchRoleList: React.PropTypes.func,
     fetchGroupList: React.PropTypes.func,
     assignGroup: React.PropTypes.func,
+    unassignGroup: React.PropTypes.func,
   }
 
   constructor(props) {
@@ -94,13 +95,28 @@ export class ProjectUserCreateContainer extends React.Component {
       email,
       role: { name: roleName },
     }
+    const { user, groupList } = this.props
     Promise.resolve(this.props.updateProjectUser(this.props.params.user_id, updatedUser))
       .then((actionResult) => {
         if (!actionResult.error) {
-          Promise.resolve(
-            forEach(group, currentGroup => this.props.assignGroup(currentGroup, email)),
-          ).then((actionResult2) => {
-            if (!actionResult2.error) {
+          // Retrieve new group
+          const addUserToGroupTasks = chain(group)
+            .filter(currentGroup => every(groupList[currentGroup].content.users, userInfo =>
+              userInfo.email !== email,
+              ))
+            .map(currentGroup => {
+              console.log(currentGroup)
+              return this.props.assignGroup(currentGroup, email)
+            })
+            .value()
+          const removeUserFromGroupTasks = chain(groupList)
+            .filter(currentGroup => some(currentGroup.content.users, userInfo => userInfo.email === email)
+                && every(group, groupName => groupName !== currentGroup.content.name))
+            .map(currentGroup => this.props.unassignGroup(currentGroup.content.name, email))
+            .value()
+          const tasks = concat(addUserToGroupTasks, removeUserFromGroupTasks)
+          Promise.all(tasks).then((actionResults) => {
+            if (every(actionResults, (actionResult) => actionResult.error)) {
               const url = this.getBackUrl()
               browserHistory.push(url)
             }
@@ -135,6 +151,7 @@ export class ProjectUserCreateContainer extends React.Component {
     })
   }
 
+  // TODO ajouter le composant de waiting des données pour que le fetch des groupes ait le temps de se faire
   render() {
     return (
       <I18nProvider messageDir="modules/admin-user-projectuser-management/src/i18n">
@@ -156,6 +173,7 @@ const mapDispatchToProps = dispatch => ({
   fetchRoleList: () => dispatch(RoleActions.fetchEntityList()),
   fetchGroupList: () => dispatch(AccessGroupActions.fetchPagedEntityList()),
   assignGroup: (group, user) => dispatch(UserGroupActions.sendSignal('PUT', null, { name: group, email: user })),
+  unassignGroup: (group, user) => dispatch(UserGroupActions.sendSignal('DELETE', null, { name: group, email: user })),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectUserCreateContainer)
