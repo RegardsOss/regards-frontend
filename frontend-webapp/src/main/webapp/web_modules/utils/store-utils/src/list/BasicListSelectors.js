c570c35b99e0fa0753ce0f1e27cdc4c14237717e
sbinda <sebastien.binda@c-s.fr>
2017-05-04 09:50:29 +0200

    [BASIC LIST SELECTORS Fix mapStateToProps for performances.

--- frontend-webapp/src/main/webapp/web_modules/utils/store-utils/src/list/BasicListSelectors.js
+++ frontend-webapp/src/main/webapp/web_modules/utils/store-utils/src/list/BasicListSelectors.js
@@ -3,8 +3,7 @@
  **/
 import size from 'lodash/size'
 import map from 'lodash/map'
-
-
+import {createSelector} from 'reselect'
 /**
  * @author Léo Mieulet
  */
@@ -13,15 +12,23 @@ import BasicSelector from '../BasicSelector'
  *  Provide an high level class to interact with entity stored in a list
  */
 class BasicListSelectors extends BasicSelector {
-  getList(state) {
+
+  getList = (state) => {
     return this.uncombineStore(state).items
   }
-  getOrderedList(state) {
-    return map(this.uncombineStore(state).results, entityId => this.getById(state, entityId))
+
+  getResults = (state) => {
+    return this.uncombineStore(state).results
   }
+
+  getOrderedList = createSelector([this.getResults, this.getList],
+    (orderedResults, list) => map(orderedResults, entityId => list[entityId])
+  )
+
   getById(state, id) {
     return this.uncombineStore(state).items[id]
   }
+
   getContentById(state, id) {
     const response = this.getById(state, id)
     if (response && response.content) {
@@ -29,6 +36,7 @@ class BasicListSelectors extends BasicSelector {
     }
     return undefined
   }
+
   getSize(state) {
     return size(this.uncombineStore(state).items)
   }
