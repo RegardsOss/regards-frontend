b6b3ee1bb357fe7ec2d865b7372f67b7805de23c
RaphaelMechali <raphael.mechali@gmail.com>
2017-05-05 19:10:00 +0200

    UI services (first part)
    - Added the single dataset ipId configuration for search results (not dynamic)
    - Refactored drop down button

---
+++ frontend-webapp/src/main/webapp/web_modules/components/src/buttons/DropDownButton.jsx
@@ -0,0 +1,93 @@
+/**
+* LICENSE_PLACEHOLDER
+**/
+import FlatButton from 'material-ui/FlatButton'
+import Popover from 'material-ui/Popover'
+import Menu from 'material-ui/Menu'
+import DrowDownIcon from 'material-ui/svg-icons/navigation/arrow-drop-down'
+
+/**
+* Drop down button (used where drop down menu is not adequate)
+*/
+class DropDownButton extends React.Component {
+
+  static propTypes = {
+    ButtonConstructor: React.PropTypes.func.isRequired,
+    getLabel: React.PropTypes.func.isRequired, // Generates label: (current value (optional)) => string
+    children: React.PropTypes.node.isRequired, // Expected children: menu items
+    onChange: React.PropTypes.func, // on change listener
+    // eslint-disable-next-line react/forbid-prop-types
+    value: React.PropTypes.any,
+  }
+
+  static defaultProps = {
+    ButtonConstructor: FlatButton,
+  }
+
+  componentWillMount = () => {
+    this.setMenuVisibleOn()
+    this.setCurrentValue(this.props.value)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) {
+      // externally controlled value change
+      this.setCurrentValue(nextProps.value)
+    }
+  }
+
+
+  onOpenMenu = (event) => {
+    event.preventDefault()
+    this.setMenuVisibleOn(event.currentTarget)
+  }
+
+  onCloseMenu = () => this.setMenuVisibleOn()
+
+  onMenuItemSelected = (event, value) => {
+    // hide menu
+    this.setMenuVisibleOn()
+    // keep selected value
+    this.setCurrentValue(value)
+    // relay event
+    const { onChange } = this.props
+    if (onChange) {
+      onChange(value)
+    }
+  }
+
+  setMenuVisibleOn = (menuVisibleOn = null) => {
+    if (!this.state || this.state.menuVisibleOn !== menuVisibleOn) {
+      this.setState({ menuVisibleOn })
+    }
+  }
+
+  setCurrentValue = value => this.setState({ value })
+
+  render() {
+    const { ButtonConstructor, getLabel, children } = this.props
+    const { value, menuVisibleOn } = this.state
+    return (
+      <div>
+        <ButtonConstructor
+          label={getLabel(value)}
+          onTouchTap={this.onOpenMenu}
+          labelPosition="before"
+          icon={<DrowDownIcon />}
+        />
+        <Popover
+          open={!!menuVisibleOn}
+          anchorEl={this.state.menuVisibleOn}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          onRequestClose={this.onCloseMenu}
+        >
+          <Menu onChange={this.onMenuItemSelected}>
+            {children}
+          </Menu>
+        </Popover>
+      </div>
+    )
+  }
+}
+export default DropDownButton
