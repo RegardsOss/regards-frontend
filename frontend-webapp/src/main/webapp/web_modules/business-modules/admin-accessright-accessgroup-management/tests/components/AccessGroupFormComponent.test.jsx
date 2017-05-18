6d65da015b0940c2c86b45fd2e84bee5a10620bf
sbinda <sebastien.binda@c-s.fr>
2017-05-18 12:47:07 +0200

    Add tests for accessgroupListComponent

--- frontend-webapp/src/main/webapp/web_modules/business-modules/admin-accessright-accessgroup-management/tests/components/AccessGroupFormComponent.test.jsx
+++ frontend-webapp/src/main/webapp/web_modules/business-modules/admin-accessright-accessgroup-management/tests/components/AccessGroupFormComponent.test.jsx
@@ -3,32 +3,20 @@
  */
 import { shallow } from 'enzyme'
 import { expect, assert } from 'chai'
-import { stub } from 'sinon'
-import { IntlStub } from '@regardsoss/tests-helpers'
+import { testSuiteHelpers, buildTestContext } from '@regardsoss/tests-helpers'
 import { Field } from '@regardsoss/form-utils'
 import { AccessGroupFormComponent } from '../../src/components/AccessGroupFormComponent'
 
+const context = buildTestContext()
+
 describe('[ADMIN USER ACCESSGROUP MANAGEMENT] Testing AccessGroupFormComponent', () => {
-  // Since react will console.error propType warnings, that which we'd rather have
-  // as errors, we use sinon.js to stub it into throwing these warning as errors
-  // instead.
-  before(() => {
-    stub(console, 'error').callsFake((warning) => {
-      throw new Error(warning)
-    })
-  })
-  after(() => {
-    console.error.restore()
-  })
+  before(testSuiteHelpers.before)
+  after(testSuiteHelpers.after)
+
   it('should exists', () => {
     assert.isDefined(AccessGroupFormComponent)
   })
-  const context = {
-    intl: IntlStub,
-    muiTheme: {
-      palette: {},
-    },
-  }
+
   it('Render properly', () => {
     const props = {
       onSubmit: () => {},
--- frontend-webapp/src/main/webapp/web_modules/business-modules/admin-accessright-accessgroup-management/tests/containers/AccessGroupListContainer.test.jsx
+++ frontend-webapp/src/main/webapp/web_modules/business-modules/admin-accessright-accessgroup-management/tests/containers/AccessGroupListContainer.test.jsx
@@ -3,33 +3,21 @@
  */
 import { shallow } from 'enzyme'
 import { expect, assert } from 'chai'
-import { stub } from 'sinon'
-import { IntlStub, DumpProvider } from '@regardsoss/tests-helpers'
+import { testSuiteHelpers, DumpProvider, buildTestContext } from '@regardsoss/tests-helpers'
 import { LoadableContentDisplayDecorator } from '@regardsoss/display-control'
 import { AccessGroupListContainer } from '../../src/containers/AccessGroupListContainer'
 
+const context = buildTestContext()
+
 describe('[ADMIN USER ACCESSGROUP MANAGEMENT] Testing AccessGroupListContainer', () => {
-  // Since react will console.error propType warnings, that which we'd rather have
-  // as errors, we use sinon.js to stub it into throwing these warning as errors
-  // instead.
-  before(() => {
-    stub(console, 'error').callsFake((warning) => {
-      throw new Error(warning)
-    })
-  })
-  after(() => {
-    console.error.restore()
-  })
+  before(testSuiteHelpers.before)
+  after(testSuiteHelpers.after)
+
   it('should exists', () => {
     assert.isDefined(AccessGroupListContainer)
     assert.isDefined(LoadableContentDisplayDecorator)
   })
-  const context = {
-    intl: IntlStub,
-    muiTheme: {
-      palette: {},
-    },
-  }
+
   it('Render properly', () => {
     const props = {
       params: {
