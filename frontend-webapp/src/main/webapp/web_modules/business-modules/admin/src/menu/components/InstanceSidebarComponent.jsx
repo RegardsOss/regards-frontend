2156dd7dc30e932c37eae2e194def9ae68f8cd75
lmieulet <leo.mieulet@c-s.fr>
2017-05-23 18:51:03 +0200

    lint components and admin

--- frontend-webapp/src/main/webapp/web_modules/business-modules/admin/src/menu/components/InstanceSidebarComponent.jsx
+++ frontend-webapp/src/main/webapp/web_modules/business-modules/admin/src/menu/components/InstanceSidebarComponent.jsx
@@ -44,43 +44,43 @@ class InstanceSidebarComponent extends React.Component {
     const moduleStyles = getModuleStyles(muiTheme)
 
     return (
-        <Drawer
-          open
-          containerStyle={moduleStyles.adminApp.layout.sidebarContainer.styles}
-          className={moduleStyles.adminApp.layout.sidebarContainer.classes.join(' ')}
-        >
-          <HateoasSidebarElement
-            endpointKey="projects_url"
-            key="0"
-            to={'/admin/projects/list'}
-            currentPath={this.props.currentPath}
-            primaryText={this.context.intl.formatMessage({ id: 'menu.projects' })}
-            leftIcon={<Settings />}
-          />
-          <HateoasSidebarElement
-            endpointKey="projects_users_url"
-            key="1"
-            to={'/admin/account/list'}
-            currentPath={this.props.currentPath}
-            primaryText={this.context.intl.formatMessage({ id: 'menu.accounts' })}
-            leftIcon={<SupervisorAccount />}
-            rightIcon={<WaitingAccountsNotificationContainer />}
-          />
-          <HateoasSidebarElement
-            endpointKey="projects_users_url"
-            key="3"
-            to={'/admin/ui/board'}
-            currentPath={this.props.currentPath}
-            primaryText={this.context.intl.formatMessage({ id: 'menu.instance.ui.configuration' })}
-            leftIcon={<Brush />}
-          />
-          <Divider />
-          <MenuItem
-            primaryText={this.context.intl.formatMessage({ id: 'menu.logout' })}
-            leftIcon={<PowerSettingsNew />}
-            onTouchTap={onLogout}
-          />
-        </Drawer>
+      <Drawer
+        open
+        containerStyle={moduleStyles.adminApp.layout.sidebarContainer.styles}
+        className={moduleStyles.adminApp.layout.sidebarContainer.classes.join(' ')}
+      >
+        <HateoasSidebarElement
+          endpointKey="projects_url"
+          key="0"
+          to={'/admin/projects/list'}
+          currentPath={this.props.currentPath}
+          primaryText={this.context.intl.formatMessage({ id: 'menu.projects' })}
+          leftIcon={<Settings />}
+        />
+        <HateoasSidebarElement
+          endpointKey="projects_users_url"
+          key="1"
+          to={'/admin/account/list'}
+          currentPath={this.props.currentPath}
+          primaryText={this.context.intl.formatMessage({ id: 'menu.accounts' })}
+          leftIcon={<SupervisorAccount />}
+          rightIcon={<WaitingAccountsNotificationContainer />}
+        />
+        <HateoasSidebarElement
+          endpointKey="projects_users_url"
+          key="3"
+          to={'/admin/ui/board'}
+          currentPath={this.props.currentPath}
+          primaryText={this.context.intl.formatMessage({ id: 'menu.instance.ui.configuration' })}
+          leftIcon={<Brush />}
+        />
+        <Divider />
+        <MenuItem
+          primaryText={this.context.intl.formatMessage({ id: 'menu.logout' })}
+          leftIcon={<PowerSettingsNew />}
+          onTouchTap={onLogout}
+        />
+      </Drawer>
     )
   }
 }
