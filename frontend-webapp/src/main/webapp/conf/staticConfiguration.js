e60b586fca00e536f96d420ec1c6fe1d22657d2e
sbinda <sebastien.binda@c-s.fr>
2017-05-29 15:13:03 +0200

    Fix microservices list.

--- frontend-webapp/src/main/webapp/conf/staticConfiguration.js
+++ frontend-webapp/src/main/webapp/conf/staticConfiguration.js
@@ -18,7 +18,7 @@ GATEWAY_HOSTNAME='http://${regards.config.cloud.gateway.uri}:${regards.config.cl
  */
 STATIC_CONFIGURATION= {
   // Available microservices from backend server.
-  microservices: ['rs-access-project','rs-admin','rs-authentication','rs-catalog','rs-dam','rs-gateway'],
+  microservices: ['rs-access-project','rs-admin','rs-authentication','rs-catalog','rs-dam'],
   // Default driver used to create a project connection (see module admin-database-management)
   projectConnectionDriver: 'org.postgresql.Driver'
 }
