8ef697096ff22743823c9a820e79011d5135d84d
Raphaël Mechali <raphael.mechali@c-s.fr>
2017-09-04 13:57:54 +0200

    fix eslint issue with CNES comments

--- frontend-webapp/src/main/webapp/eslint-config-es6-rules/index.js
+++ frontend-webapp/src/main/webapp/eslint-config-es6-rules/index.js
@@ -1,4 +1,5 @@
-'use strict'
+
+
 /**
  * Copyright 2017 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
@@ -19,113 +20,113 @@
  **/
 
 
-var allRules = {
+const allRules = {
   // eslint rules configuration
   // Allow annonymous functions
-  "func-names": 0,
-  "global-require": 0,
-  "max-len": 0,
-  "no-console": 0,
+  'func-names': 0,
+  'global-require': 0,
+  'max-len': 0,
+  'no-console': 0,
   // Do not check comments otherwise every files needs (License more exactly) would be linted
-  "spaced-comment": 0,
+  'spaced-comment': 0,
   // ignore unused variable on functions arguments
-  "no-unused-vars": [ 2, { "args": "none" } ],
-  "no-confusing-arrow": 0,
-  "no-warning-comments" : [1, { "terms": ["todo", "fixme"], "location": "anywhere" }],
-  "semi": [
-    "error",
-    "never"
+  'no-unused-vars': [2, { args: 'none' }],
+  'no-confusing-arrow': 0,
+  'no-warning-comments': [1, { terms: ['todo', 'fixme'], location: 'anywhere' }],
+  semi: [
+    'error',
+    'never',
   ],
 
   // Airbnb rules configuration
-  "import/no-extraneous-dependencies": 0,
-  "import/no-named-as-default": 0,
-  "import/no-named-as-default-member": 0,
+  'import/no-extraneous-dependencies': 0,
+  'import/no-named-as-default': 0,
+  'import/no-named-as-default-member': 0,
 
   // React rules configuration
-  "react/jsx-uses-react": 2,
-  "react/jsx-uses-vars": 2,
-  "react/react-in-jsx-scope": 2,
-  "react/prefer-es6-class": 0,
-  "react/require-extension": 0,
-  "react/require-default-props": 0,
-  "react/prefer-stateless-function": 0,
-  "react/display-name" : 0,
+  'react/jsx-uses-react': 2,
+  'react/jsx-uses-vars': 2,
+  'react/react-in-jsx-scope': 2,
+  'react/prefer-es6-class': 0,
+  'react/require-extension': 0,
+  'react/require-default-props': 0,
+  'react/prefer-stateless-function': 0,
+  'react/display-name': 0,
 
   // Lodash rules configuration
-  "lodash/import-scope": [ 2, "method" ],
+  'lodash/import-scope': [2, 'method'],
   // Disable useless rule (_.noop instead of empty annonnymous function)
-  "lodash/prefer-noop": 0,
+  'lodash/prefer-noop': 0,
   // This rule is not satisfying
-  "lodash/prefer-lodash-method": 0,
+  'lodash/prefer-lodash-method': 0,
   // We do not use lodash constants factory
-  "lodash/prefer-constant": 0,
+  'lodash/prefer-constant': 0,
   // Not so lisible
-  "lodash/prop-shorthand": 0,
+  'lodash/prop-shorthand': 0,
   // We prefer the explicit declaration
-  "lodash/matches-prop-shorthand": 0,
+  'lodash/matches-prop-shorthand': 0,
 
   // Mocha rules configuration
   // See https://github.com/lo1tuma/eslint-plugin-mocha/tree/master/docs/rules
-  "mocha/no-exclusive-tests": 2,
-  "mocha/max-top-level-suites": 2,
-  "mocha/handle-done-callback": 2,
-  "mocha/no-top-level-hooks": 2,
-  "mocha/no-return-and-callback": 2,
-  "mocha/no-sibling-hooks": 2,
-  "mocha/no-global-tests": 2,
+  'mocha/no-exclusive-tests': 2,
+  'mocha/max-top-level-suites': 2,
+  'mocha/handle-done-callback': 2,
+  'mocha/no-top-level-hooks': 2,
+  'mocha/no-return-and-callback': 2,
+  'mocha/no-sibling-hooks': 2,
+  'mocha/no-global-tests': 2,
   // All React tests contains before and after whether having 1 or more tests
-  "mocha/no-hooks-for-single-case": 0,
-  "mocha/no-mocha-arrows": 0,
+  'mocha/no-hooks-for-single-case': 0,
+  'mocha/no-mocha-arrows': 0,
   // We do not use mocha context so we can use lambdas
-  "mocha/no-nested-tests": 2,
-  "mocha/no-skipped-tests": 2,
-  "mocha/no-identical-title": 2,
+  'mocha/no-nested-tests': 2,
+  'mocha/no-skipped-tests': 2,
+  'mocha/no-identical-title': 2,
   // We wrote sync tests
-  "mocha/no-synchronous-tests": 0,
+  'mocha/no-synchronous-tests': 0,
 
   // Promise rules configuration
   // We call done from Mocha inside async tests
-  "promise/no-callback-in-promise": 0,
+  'promise/no-callback-in-promise': 0,
   // Rule is not adequate without async / await
-  "promise/no-nesting": 0,
+  'promise/no-nesting': 0,
   // This is handy to create custom promises!
-  "promise/avoid-new": 0,
+  'promise/avoid-new': 0,
   // We do not always return a promise on react component since most of the time nobody consumes them
-  "promise/catch-or-return": 0,
-  "promise/always-return": 0
+  'promise/catch-or-return': 0,
+  'promise/always-return': 0,
 }
 
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:react-perf/recommended",
-    "plugin:lodash/recommended",
-    "plugin:promise/recommended"
+    'eslint:recommended',
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:react-perf/recommended',
+    'plugin:lodash/recommended',
+    'plugin:promise/recommended',
   ],
   plugins: [
-    "react",
-    "lodash",
-    "mocha",
-    "promise",
-    "react-perf"
+    'react',
+    'lodash',
+    'mocha',
+    'promise',
+    'react-perf',
   ],
   env: {
-    "browser": true,
-    "mocha": true,
-    "node": true,
-    "es6": true,
-    "commonjs": true
+    browser: true,
+    mocha: true,
+    node: true,
+    es6: true,
+    commonjs: true,
   },
   globals: {
-    "React": true,
-    "PropTypes": true,
+    React: true,
+    PropTypes: true,
   },
   parserOptions: {
-    "ecmaVersion": 6,
-    "sourceType": "module"
+    ecmaVersion: 6,
+    sourceType: 'module',
   },
-  rules: allRules
+  rules: allRules,
 }
