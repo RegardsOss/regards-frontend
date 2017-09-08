aba73c75ff3646bcbdc3864cb882f81bb9374caa
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-09-08 11:51:08 +0200

    Correct buld script

--- test/node/build_webapp.sh
+++ test/node/build_webapp.sh
@@ -2,23 +2,8 @@
 
 cd /app_to_build
 
-echo "execute preinstallation (bootstrap)"
-npm run bootstrap
-
-echo "execute npm install"
-npm install
-
-# echo "execute tests"
+echo "Execute tests"
 # npm run test:mocha
 
-# echo "Building app"
-# npm run build:production
-
-# echo "build app and plugins"
-# npm run build:plugins
-
-echo "Fix permissions"
-chmod -R 0777 /app_to_build/dist/prod
-
-echo "Success !"
-exit 0
+echo "Building app"
+npm run build:production
