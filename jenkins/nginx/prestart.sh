273ce6b93f6aa2b914bbd70dbc520d7c077e52e9
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-09-14 16:26:50 +0200

    Fix nginx image

--- jenkins/nginx/prestart.sh
+++ jenkins/nginx/prestart.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -xe
-
+#!/usr/bin/env bash
+set -e
 sed -i s/\$\{regards\.config\.cloud\.gateway\.url}/"$GATEWAY_PUBLIC_URL"/g /var/www/conf/staticConfiguration.js
 cat /var/www/conf/staticConfiguration.js
