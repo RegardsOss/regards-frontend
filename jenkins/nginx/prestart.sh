385280a18d5ded57edf45842fcb76a8459088b3b
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-09-14 15:35:55 +0200

    Add -xe to shebang

--- jenkins/nginx/prestart.sh
+++ jenkins/nginx/prestart.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -xe
 
 sed -i s/\$\{regards\.config\.cloud\.gateway\.url}/"$GATEWAY_PUBLIC_URL"/g /var/www/conf/staticConfiguration.js
 cat /var/www/conf/staticConfiguration.js
