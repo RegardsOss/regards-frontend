22a0b18c0a41f41588d70347af8f2686d62ae1f3
sbinda <sebastien.binda@c-s.fr>
2017-10-16 17:53:13 +0200

    Fix test and lint issues.

--- frontend-webapp/src/main/webapp/web_modules/modules/authentication/src/components/AskProjectAccessFormComponent.jsx
+++ frontend-webapp/src/main/webapp/web_modules/modules/authentication/src/components/AskProjectAccessFormComponent.jsx
@@ -50,8 +50,6 @@ export class AskProjectAccessFormComponent extends React.Component {
     onRequestAction: PropTypes.func.isRequired,
     // back
     onBack: PropTypes.func.isRequired,
-    // project name
-    project: PropTypes.string.isRequired,
     // project metadata
     projectMetadata: MetadataList.isRequired,
     // from reduxFormSelector
@@ -80,7 +78,7 @@ export class AskProjectAccessFormComponent extends React.Component {
 
   render() {
     const {
-      project, projectMetadata, passwordRules,
+      projectMetadata, passwordRules,
       currentMailValue, useExistingAccount, errorMessage,
       onBack, onRequestAction,
       pristine, submitting, invalid, handleSubmit,
