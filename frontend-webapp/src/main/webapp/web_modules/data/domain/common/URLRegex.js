80b4bdd75655aae796637a517e702fc45a953fbc
Léo Mieulet <leo.mieulet@c-s.fr>
2017-10-24 15:23:24 +0200

    Improve absolute URL regex

--- frontend-webapp/src/main/webapp/web_modules/data/domain/common/URLRegex.js
+++ frontend-webapp/src/main/webapp/web_modules/data/domain/common/URLRegex.js
@@ -21,7 +21,7 @@ const relativeURLRegexp =
 
 
 const validURLRegexp =
-  /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.?)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.:~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i
+  /^(https?:\/\/)?[a-z\d-_]+(\.[a-z\d-_]+)*(:\d+)?(\/[-a-z\d%_.:~+ ]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i
 
 export default {
   relativeURLRegexp,
