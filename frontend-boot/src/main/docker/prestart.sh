12dffb114d819afec693adcd4eddbdf75de359ed
Léo Mieulet <leo.mieulet@c-s.fr>
2017-11-13 13:23:27 +0100

    Remove legacy java files + try to fix user rights

--- frontend-boot/src/main/docker/prestart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-jar -xvf /bootstrap-frontend-2.0.0-SNAPSHOT.war static/
-sed -i s/\$\{regards\.config\.cloud\.gateway\.url}/"$GATEWAY_PUBLIC_URL"/g /static/conf/staticConfiguration.js
-cat /static/conf/staticConfiguration.js
