756e0df20849017095a5c9a5c0dfacb940fd723d
sbinda <sebastien.binda@c-s.fr>
2017-12-13 15:27:12 +0100

    Fix animcations.css styles import.

--- frontend-webapp/src/main/webapp/web_modules/business-common/order-common/src/components/orders/options/DownloadOrderFilesAsZipComponent.jsx
+++ frontend-webapp/src/main/webapp/web_modules/business-common/order-common/src/components/orders/options/DownloadOrderFilesAsZipComponent.jsx
@@ -23,10 +23,9 @@ import Chip from 'material-ui/Chip'
 import { DownloadButton, ShowableAtRender } from '@regardsoss/components'
 import { i18nContextType } from '@regardsoss/i18n'
 import { themeContextType } from '@regardsoss/theme'
-// import '../../../styles/animations.css'
 
 /** Constructor wrapper to use the IconButton within a DropDownButton */
-const IconButtonConstructorWrapper = props => <IconButton {...(omit(props, ['label', 'labelPosition'])) } />
+const IconButtonConstructorWrapper = props => <IconButton {...(omit(props, ['label', 'labelPosition']))} />
 
 /**
  * Download order files as zip table option
