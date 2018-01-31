3c5fff9baed43ef2bb7f7cfbff962ab927492672
RaphaelMechali <raphael.mechali@gmail.com>
2018-01-31 11:45:56 +0100

    regards/regards#198 some more correction

--- frontend-webapp/src/main/webapp/web_modules/data/store/src/SessionLockedMiddleware.js
+++ frontend-webapp/src/main/webapp/web_modules/data/store/src/SessionLockedMiddleware.js
@@ -16,20 +16,61 @@
  * You should have received a copy of the GNU General Public License
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  **/
+import get from 'lodash/get'
+import isString from 'lodash/isString'
+
 const { CALL_API } = require('redux-api-middleware')
+
+const EMPTY_ARRAY = []
+const COMMON_AUTHENTICATION_NAMESPACE = 'common/authentication-manager'
+
+/**
+ * Is action CALL API as parameter an authentication request
+ * @param {*} callAPI callAPI part of the action
+ * @return {boolean} true if action is a call API action
+ */
+function isAuthenticationRequest(callAPI) {
+  return get(callAPI, 'types', EMPTY_ARRAY).find(typeOrObject =>
+    isString(typeOrObject) ?
+      typeOrObject.startsWith(COMMON_AUTHENTICATION_NAMESPACE) :
+      typeOrObject.type.startsWith(COMMON_AUTHENTICATION_NAMESPACE))
+}
+
+/**
+ * Determinates if the action should be blocked as session is locked
+ * @param {*} action redux action to test
+ * @param {*}
+ * @return {boolean} true when action is a fetch action and session is locked (blocking case)
+ */
+function shouldBlockAction(action, store) {
+  const callAPI = action[CALL_API]
+  // 1 - Is it a fetch action (but not authentication fetch)? (ie: is it a CALL API action but not an authentication action)
+  if (!callAPI || isAuthenticationRequest(callAPI)) {
+    // No: it must be handled no matter the session state
+    return false
+  }
+  // 2 - Is session locked
+  // Note: we need here to recompute it, as brwosers timers are broken when Operating System session is locked (see Sess)
+  // 2.a - is user authenticated?
+  const reduxState = store.getState()
+  const authentication = get(reduxState, 'common.authentication', null)
+  const authenticationDate = get(authentication, 'authenticateDate', null)
+  const expiresInSeconds = get(authentication, 'result.expires_in', null)
+  if (!authenticationDate || !expiresInSeconds) {
+    // Not authenticated => session cannot be locked
+    return false
+  }
+
+  // 2.b - user is authenticated => session is locked if token expired
+  return authenticationDate + (expiresInSeconds * 1000) - Date.now() <= 0
+}
 
 // Intercept actions to reject them if the current user sessions is expired (locked)
 const SessionLockedMiddleware = store => next => (action) => {
-  const currentState = store.getState()
-  const callAPI = action[CALL_API]
-
   // If the action is a callAPI and the session of current authenticated user is locked do not send request to server.
-  if (callAPI && currentState && currentState.common && currentState.common.authentication && currentState.common.authentication.sessionLocked) {
-    if (callAPI.types && !callAPI.types.includes('common/authentication-manager/REQUEST')) {
-      return new Promise((resolve, reject) => resolve({}))
-    }
+  if (shouldBlockAction(action, store)) {
+    return new Promise((resolve, reject) => resolve({}))
   }
-
   return next(action)
 }
 
