d331a0d321f434e93c0012c92c2e8c79ad5253b7
sbinda <sebastien.binda@c-s.fr>
2018-04-16 10:57:19 +0200

    Fix frontend compilation for production.

--- webapp/webpack-config-front/src/utils/addProdPlugins.js
+++ webapp/webpack-config-front/src/utils/addProdPlugins.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
 const merge = require('./merge')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 // Remove the a folder before building
 module.exports = function (oldConf) {
@@ -16,23 +16,26 @@ module.exports = function (oldConf) {
       new webpack.optimize.AggressiveMergingPlugin(),
       new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 10000 }),
       // Minimize all JavaScript output of chunks
-      /*
-      DESACTIVATED FOR REGARDS V1
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          screw_ie8: true, // React doesn't support IE8
-          warnings: false,
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false, // remove comments
+          },
+          compress: {
+            unused: true,
+            dead_code: true, // big one--strip code that will never execute
+            warnings: false, // good for prod apps so users can't peek behind curtain
+            drop_debugger: true,
+            conditionals: true,
+            evaluate: true,
+            drop_console: true, // strips console statements
+            sequences: true,
+            booleans: true,
+          },
+          // Do not generate source map files (this is usefull during developpment)
+          sourceMap: false,
         },
-        mangle: {
-          screw_ie8: true,
-        },
-        output: {
-          comments: false,
-          screw_ie8: true,
-        },
-        // Do not generate source map files (this is usefull during developpment)
-        sourceMap: false,
-      }),*/
+      }),
       new webpack.BannerPlugin('Copyright CNES'),
     ],
   })
