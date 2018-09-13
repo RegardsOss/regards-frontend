ae9426c85e69706a0e83293cfef53d9169196bf3
Léo Mieulet <leo.mieulet@c-s.fr>
2018-09-13 15:06:47 +0200

    wtf gitkraken - fix tests

---
+++ webapp/web_modules/business-common/entities-common/src/main.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ **/
+// Description
+export { default as DescriptionProviderContainer } from './containers/description/DescriptionProviderContainer'
+export { buildDescriptionModuleConsumerID } from './definitions/description/DescriptionConsumerID'
+
+// Services
+export { default as ServiceContainer } from './containers/services/ServiceContainer'
+export { PluginServiceRunModel } from './definitions/PluginServiceRunModel'
+export { default as target } from './definitions/ServiceTarget'
+
+export { default as BooleanParameterField } from './components/services/parameters/BooleanParameterField'
+export { default as ChoiceParameterField } from './components/services/parameters/ChoiceParameterField'
+export { default as DateParameterField } from './components/services/parameters/DateParameterField'
+export { default as TextParameterField } from './components/services/parameters/TextParameterField'
