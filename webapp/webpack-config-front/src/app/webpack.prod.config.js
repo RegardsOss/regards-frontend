1584ced74ddc7063b0b52e69b54c73a257de1226
sbinda <sebastien.binda@c-s.fr>
2019-06-04 09:14:34 +0200

    Fix compilation.

--- webapp/webpack-config-front/src/app/webpack.prod.config.js
+++ webapp/webpack-config-front/src/app/webpack.prod.config.js
@@ -2,7 +2,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const path = require('path')
-const StatsPlugin = require('stats-webpack-plugin')
 const getCommonConfig = require('./webpack.common.config')
 
 module.exports = function (projectContextPath) {
@@ -77,9 +76,6 @@ module.exports = function (projectContextPath) {
         // eslint-disable-next-line import/no-dynamic-require
         manifest: require(`${projectContextPath}/dist/prod/coreoss-manifest.json`),
         context: projectContextPath,
-      }),
-      new StatsPlugin(`../../reports/prod-${Date.now()}-profile.json`, {
-        chunkModules: true,
       }),
     ],
   })
