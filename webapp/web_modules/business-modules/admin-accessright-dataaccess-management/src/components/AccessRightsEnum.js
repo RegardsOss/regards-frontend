0857c7206c03d8ceeab16d49b5c58ed50265c4d3
Simon MILHAU <simon.milhau@outlook.com>
2019-07-16 11:13:42 +0200

    Fix partial objects access right : #189762

--- webapp/web_modules/business-modules/admin-accessright-dataaccess-management/src/components/AccessRightsEnum.js
+++ webapp/web_modules/business-modules/admin-accessright-dataaccess-management/src/components/AccessRightsEnum.js
@@ -33,8 +33,6 @@ const DATA_ACCESS_ENUM = {
   AUTHORIZED: 'INHERITED_ACCESS',
   // Access denied to files of dataobjets
   REFUSED: 'NO_ACCESS',
-  // Custom access to files of dataobjects. Cusstom means that the access is defined by a plugin
-  AUTHORIZED_BY_PLUGIN: 'CUSTOM_ACCESS',
 }
 
 const QUALITY_LEVEL_ENUM = {
