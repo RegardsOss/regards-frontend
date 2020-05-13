196bffd22586f919b849bfc62b0b690c909d5172
RaphaelMechali <raphael.mechali@gmail.com>
2020-05-13 19:44:45 +0200

    search-form module removal PFSHHHHHHHH

--- webapp/web_modules/modules/search-results/tests/i18n/messages.test.js
+++ webapp/web_modules/modules/search-results/tests/i18n/messages.test.js
@@ -23,7 +23,7 @@ import MessagesEN from '../../src/i18n/messages.en.i18n'
 import MessagesFR from '../../src/i18n/messages.fr.i18n'
 
 /**
- * Tests for i18n messages of search-form module
+ * Tests for i18n messages of search-results module
  * @author Sébastien binda
  */
 describe('[SEARCH RESULTS] Testing i18n', () => {
