0de04bd378dc3eea002b094f5613183c4446f606
sbinda <sebastien.binda@c-s.fr>
2020-06-23 14:18:21 +0200

    Allow donwloads for referenced files.

--- webapp/web_modules/modules/search-results/src/components/user/tabs/results/common/options/DownloadEntityFileComponent.jsx
+++ webapp/web_modules/modules/search-results/src/components/user/tabs/results/common/options/DownloadEntityFileComponent.jsx
@@ -73,7 +73,7 @@ class DownloadEntityFileComponent extends React.Component {
     const { entity: { content: { files } } } = this.props
     // retrieve each downloadable file, ie for each downloadable file type, find online files (documents and
     // external files are always online)
-    return DownloadEntityFileComponent.DOWNLOADABLE_FILES_TYPES.reduce((acc, fileType) => [...acc, ...get(files, fileType, []).filter(f => f.online)], [])
+    return DownloadEntityFileComponent.DOWNLOADABLE_FILES_TYPES.reduce((acc, fileType) => [...acc, ...get(files, fileType, []).filter(f => f.online || f.reference)], [])
   }
 
   /**
