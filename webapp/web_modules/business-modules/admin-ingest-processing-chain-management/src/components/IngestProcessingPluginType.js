a58e754cffea0f928314a0b22545c81fee54e650
Léo Mieulet <leo.mieulet@c-s.fr>
2020-08-24 13:47:58 +0200

    Fix ISipPostprocessing interface to configure Ingest chain

--- webapp/web_modules/business-modules/admin-ingest-processing-chain-management/src/components/IngestProcessingPluginType.js
+++ webapp/web_modules/business-modules/admin-ingest-processing-chain-management/src/components/IngestProcessingPluginType.js
@@ -25,7 +25,7 @@ const ingestPluginTypes = {
   VALIDATION: 'fr.cnes.regards.modules.ingest.domain.plugin.ISipValidation',
   GENERATION: 'fr.cnes.regards.modules.ingest.domain.plugin.IAipGeneration',
   TAG: 'fr.cnes.regards.modules.ingest.domain.plugin.IAipTagging',
-  POST_PROCESSING: 'fr.cnes.regards.modules.ingest.domain.plugin.ISipPreprocessing',
+  POST_PROCESSING: 'fr.cnes.regards.modules.ingest.domain.plugin.ISipPostprocessing',
 }
 
 export default ingestPluginTypes
