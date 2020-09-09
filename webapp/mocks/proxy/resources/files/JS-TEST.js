e6800c386ab4b89570dfe0911d71c4ec7588fa68
TheoLasserre <theo.lasserre@csgroup.eu>
2020-09-09 08:31:20 +0200

    Ignore mocks

--- webapp/mocks/proxy/resources/files/JS-TEST.js
+++
@@ -1,8 +0,0 @@
-/* This is a JS test file */
-
-function hellowWord(){
-  console.error('Say hello OK!!!!')
-}
-
-
-
--- webapp/mocks/proxy/server-vm-preprod.js
+++
@@ -1,4 +0,0 @@
-const { start } = require('./server-main')
-
-const proxyURL = 'http://172.26.47.107:9030'
-start(proxyURL)
