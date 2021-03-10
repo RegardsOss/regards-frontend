accd76fceb4f5be016c40e767d7d7a96a5931bfd
lmieulet <leo.mieulet@csgroup.eu>
2021-03-10 12:21:41 +0100

    Install package concurrently - bump node to 15 - npm install won't install prod DLL anymore

---
+++ jenkins/node/install-plugin.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright 2017-2021 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+#
+# This file is part of REGARDS.
+#
+# REGARDS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# REGARDS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+set -e
+
+cd /app_to_build/plugins/$1/$2
+
+if [ -d "node_modules/@regardsoss" ]; then
+  echo "> Clear previous dependencies to regards"
+  rm -rf "node_modules/@regardsoss"
+fi
+pwd
+npm install --prefer-offline
--- jenkins/node/install.sh
+++ jenkins/node/install.sh
@@ -5,8 +5,8 @@ cd /app_to_build
 echo "Install application dependencies and create DLL"
 npm install --prefer-offline
 
-echo "Install plugins dependencies"
-npm run install:plugins
+echo "Create production DLL"
+npm run build:production-dll
 
 echo "Success !"
 exit 0
