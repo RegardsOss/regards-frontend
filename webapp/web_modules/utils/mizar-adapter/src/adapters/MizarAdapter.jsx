2be96e6c7967572c47bd7bb1a28f30448bfe24d2
lmieulet <leo.mieulet@csgroup.eu>
2021-03-11 15:06:45 +0100

    clean code - switch to regards-mizar NPM dependency

--- webapp/web_modules/utils/mizar-adapter/src/adapters/MizarAdapter.jsx
+++ webapp/web_modules/utils/mizar-adapter/src/adapters/MizarAdapter.jsx
@@ -23,10 +23,8 @@ import find from 'lodash/find'
 import filter from 'lodash/filter'
 import last from 'lodash/last'
 import { UIDomain } from '@regardsoss/domain'
-// import './MizarLoader'
-// import './rconfig'
 import './Mizar.css'
-import Mizar from 'mizar'
+import Mizar from 'regards-mizar'
 import polygonCenter from 'geojson-polygon-center'
 import { UIShapes } from '@regardsoss/shape'
 import { GeoJsonFeaturesCollection, GeoJsonFeature } from '../shapes/FeaturesCollection'
@@ -81,113 +79,6 @@ export default class MizarAdapter extends React.Component {
   // XXX : Workaround
   static MIZAR_Y_OFFSET = 150
 
-  /**
-   * Transforsm points into a box with {min/max}{X/Y} fields and empty information field
-   * @param {[number]} point1 first point as coordinates array
-   * @param {[number]} point2 second point as coordinates array
-   * @return {{empty: boolean, minX: number, maxX: number,minY: number, maxY: number}} transformed box, never null, with all fields provided
-   */
-  static toBoxCoordinates(point1, point2) {
-    if (point1 && point2) {
-      const [p1X, p1Y] = point1
-      const [p2X, p2Y] = point2
-      const minX = Math.min(p1X, p2X)
-      const maxX = Math.max(p1X, p2X)
-      const minY = Math.min(p1Y, p2Y)
-      const maxY = Math.max(p1Y, p2Y)
-      return {
-        minX,
-        maxX,
-        minY,
-        maxY,
-        empty: minX === maxX || minY === maxY,
-      }
-    }
-    return {
-      empty: true,
-      minX: 0,
-      maxX: 0,
-      minY: 0,
-      maxY: 0,
-    }
-  }
-
-  /**
-   * Builds a feature from id and points as parameter
-   * @param {string} id
-   * @param {*} point1 first point as resolved by Mizar (array)
-   * @param {*} point2 second point as resolved by Mizar (array)
-   * @retun {*} Geo feature (matching GeoJsonFeature)
-   */
-  static toAreaFeature(featureId, point1, point2) {
-    const {
-      minX, maxX, minY, maxY, empty,
-    } = MizarAdapter.toBoxCoordinates(point1, point2)
-    if (!empty) {
-      // area is not empty
-      return {
-        id: '0',
-        type: 'Feature',
-        geometry: {
-          type: 'Polygon',
-          bbox: [minX, minY, maxX, maxY],
-          coordinates: [[[minX, minY],
-          [maxX, minY],
-          [maxX, maxY],
-          [minX, maxY],
-          [minX, minY],
-          ]],
-        },
-      }
-    }
-    return null
-  }
-
-  /**
-   * Builds a feature from id and geometry as parameter
-   * @param {string} id
-   * @param {*} geometry
-   * @retun {*} Geo feature (matching GeoJsonFeature)
-   */
-  static geometryToAreaFeature(featureId, geometry) {
-    if (geometry) {
-      const ptX = []
-      const ptY = []
-      forEach(geometry.coordinates, (coordinate) => {
-        forEach(coordinate, (coord) => {
-          if (geometry.type === 'MultiPolygon') {
-            forEach(coord, (co) => {
-              ptX.push(co[0])
-              ptY.push(co[1])
-            })
-          } else {
-            ptX.push(coord[0])
-            ptY.push(coord[1])
-          }
-        })
-      })
-      const minX = Math.min.apply(null, ptX)
-      const maxX = Math.max.apply(null, ptX)
-      const minY = Math.min.apply(null, ptY)
-      const maxY = Math.max.apply(null, ptY)
-      return {
-        id: featureId,
-        type: 'Feature',
-        geometry: {
-          type: 'Polygon',
-          bbox: [minX, minY, maxX, maxY],
-          coordinates: [[[minX, minY],
-            [maxX, minY],
-            [maxX, maxY],
-            [minX, maxY],
-            [minX, minY],
-          ]],
-        },
-      }
-    }
-    return null
-  }
-
   /** Transient instance information: keeps mizar layers and data in this as their lifecycle is correlated */
   mizar = {
     instance: null, // mizar instance
@@ -214,60 +105,60 @@ export default class MizarAdapter extends React.Component {
     * Lifecycle method: component receive props. Used here to detect properties change and update local state
     * @param {*} nextProps next component properties
     */
-   UNSAFE_componentWillReceiveProps = (nextProps) => this.onPropertiesUpdated(this.props, nextProps)
+  UNSAFE_componentWillReceiveProps = (nextProps) => this.onPropertiesUpdated(this.props, nextProps)
 
-   /**
-    * Properties change detected: update local state
-    * @param oldProps previous component properties
-    * @param newProps next component properties
-    */
-   onPropertiesUpdated = (oldProps, newProps) => {
-     const {
-       featuresCollection, drawingSelection, drawnAreas, customLayersOpacity, viewMode,
-       selectedProducts, selectedToponyms,
-     } = newProps
-     if (!isEqual(oldProps.featuresCollection, featuresCollection) || !isEqual(oldProps.selectedProducts, selectedProducts)) {
-       // Handle not selected features
-       this.onNotSelectedFeaturesUpdated(featuresCollection, selectedProducts)
-       // Handle selected feature
-       this.onSelectedFeaturesUpdated(featuresCollection, selectedProducts)
-     }
-     // remove old areas and add new ones
-     if (!isEqual(oldProps.drawnAreas, drawnAreas)) {
-       this.onAreasUpdated(oldProps.drawnAreas, drawnAreas)
-     }
-     // Handle draw mode changes
-     if (oldProps.drawingSelection !== drawingSelection) {
-       this.onToggleDrawSelectionMode(drawingSelection)
-     }
-     // remove old areas and add new ones
-     if (!isEqual(oldProps.customLayersOpacity, customLayersOpacity)) {
-       this.onUpdateOpacity(customLayersOpacity)
-     }
-     // Handle toponyms changes
-     if (!isEqual(oldProps.selectedToponyms, selectedToponyms)) {
-       this.onUpdateToponyms(selectedToponyms)
-     }
-     // Handle change view mode
-     if (!isEqual(oldProps.viewMode, viewMode)) {
-       this.onToggleViewMode()
-     }
-     // Manage camera destination
-     if (!isEqual(oldProps.selectedProducts, selectedProducts) || !isEqual(oldProps.drawnAreas, drawnAreas)) {
-       // Handle zoom on selected product
-       if (!isEqual(oldProps.selectedProducts, selectedProducts) && !isEmpty(selectedProducts)) {
-         const lastFeatureSelected = find(this.mizar.selectedFeaturesLayer.features, (feature) => feature.id === last(selectedProducts).id)
-         this.zoomOnGeometry(lastFeatureSelected.geometry)
-       } else if (!isEmpty(drawnAreas)) {
-         // When user stop drawing area or toponym change
-         if (drawingSelection === false) {
-           this.zoomOnGeometry(drawnAreas[0].geometry)
-         }
-       }
-     }
-     // XXX- take in account, in later versions, color properties change ==> requires unmounting then remounting layers
-     // useless in current version as the parent split pane blocks redrawing anyways
-   }
+  /**
+   * Properties change detected: update local state
+   * @param oldProps previous component properties
+   * @param newProps next component properties
+   */
+  onPropertiesUpdated = (oldProps, newProps) => {
+    const {
+      featuresCollection, drawingSelection, drawnAreas, customLayersOpacity, viewMode,
+      selectedProducts, selectedToponyms,
+    } = newProps
+    if (!isEqual(oldProps.featuresCollection, featuresCollection) || !isEqual(oldProps.selectedProducts, selectedProducts)) {
+      // Handle not selected features
+      this.onNotSelectedFeaturesUpdated(featuresCollection, selectedProducts)
+      // Handle selected feature
+      this.onSelectedFeaturesUpdated(featuresCollection, selectedProducts)
+    }
+    // remove old areas and add new ones
+    if (!isEqual(oldProps.drawnAreas, drawnAreas)) {
+      this.onAreasUpdated(oldProps.drawnAreas, drawnAreas)
+    }
+    // Handle draw mode changes
+    if (oldProps.drawingSelection !== drawingSelection) {
+      this.onToggleDrawSelectionMode(drawingSelection)
+    }
+    // remove old areas and add new ones
+    if (!isEqual(oldProps.customLayersOpacity, customLayersOpacity)) {
+      this.onUpdateOpacity(customLayersOpacity)
+    }
+    // Handle toponyms changes
+    if (!isEqual(oldProps.selectedToponyms, selectedToponyms)) {
+      this.onUpdateToponyms(selectedToponyms)
+    }
+    // Handle change view mode
+    if (!isEqual(oldProps.viewMode, viewMode)) {
+      this.onToggleViewMode()
+    }
+    // Manage camera destination
+    if (!isEqual(oldProps.selectedProducts, selectedProducts) || !isEqual(oldProps.drawnAreas, drawnAreas)) {
+      // Handle zoom on selected product
+      if (!isEqual(oldProps.selectedProducts, selectedProducts) && !isEmpty(selectedProducts)) {
+        const lastFeatureSelected = find(this.mizar.selectedFeaturesLayer.features, (feature) => feature.id === last(selectedProducts).id)
+        this.zoomOnGeometry(lastFeatureSelected.geometry)
+      } else if (!isEmpty(drawnAreas)) {
+        // When user stop drawing area or toponym change
+        if (drawingSelection === false) {
+          this.zoomOnGeometry(drawnAreas[0].geometry)
+        }
+      }
+    }
+    // XXX- take in account, in later versions, color properties change ==> requires unmounting then remounting layers
+    // useless in current version as the parent split pane blocks redrawing anyways
+  }
 
   /**
    * Lifecycle method: component will unmount. Used here to free loaded mizar component.
