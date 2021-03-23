6716b2902ace22e52a8f051be3d5cfa3f7cd7e97
lmieulet <leo.mieulet@csgroup.eu>
2021-03-23 15:43:13 +0100

    clean code

--- webapp/web_modules/components/src/dragndrop/DragAndDrop.jsx
+++ webapp/web_modules/components/src/dragndrop/DragAndDrop.jsx
@@ -25,104 +25,105 @@ import { withResourceDisplayControl } from '@regardsoss/display-control'
   * @author Théo Lasserre
   */
 class DragAndDrop extends React.Component {
-   static propTypes = {
-     title: PropTypes.string.isRequired,
-     subtitle: PropTypes.string.isRequired,
-     // eslint-disable-next-line react/forbid-prop-types
-     style: PropTypes.object.isRequired,
-     handleDrop: PropTypes.func.isRequired,
-     children: PropTypes.oneOfType([
-       PropTypes.arrayOf(PropTypes.node),
-       PropTypes.node,
-     ]),
-   }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    style: PropTypes.object.isRequired,
+    handleDrop: PropTypes.func.isRequired,
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+    ]),
+  }
 
-   state = {
-     dragging: false,
-   }
+  state = {
+    dragging: false,
+  }
 
-   dropRef = React.createRef()
+  dropRef = React.createRef()
 
-   componentDidMount() {
-     this.dragCounter = 0
-     const div = this.dropRef.current
-     div.addEventListener('dragenter', this.handleDragIn)
-     div.addEventListener('dragleave', this.handleDragOut)
-     div.addEventListener('dragover', this.handleDrag)
-     div.addEventListener('drop', this.handleDrop)
-   }
+  componentDidMount() {
+    this.dragCounter = 0
+    const div = this.dropRef.current
+    div.addEventListener('dragenter', this.handleDragIn)
+    div.addEventListener('dragleave', this.handleDragOut)
+    div.addEventListener('dragover', this.handleDrag)
+    div.addEventListener('drop', this.handleDrop)
+  }
 
-   componentWillUnmount() {
-     const div = this.dropRef.current
-     div.removeEventListener('dragenter', this.handleDragIn)
-     div.removeEventListener('dragleave', this.handleDragOut)
-     div.removeEventListener('dragover', this.handleDrag)
-     div.removeEventListener('drop', this.handleDrop)
-   }
+  componentWillUnmount() {
+    const div = this.dropRef.current
+    div.removeEventListener('dragenter', this.handleDragIn)
+    div.removeEventListener('dragleave', this.handleDragOut)
+    div.removeEventListener('dragover', this.handleDrag)
+    div.removeEventListener('drop', this.handleDrop)
+  }
 
-   handleDrag = (e) => {
-     e.preventDefault()
-     e.stopPropagation()
-   }
+  handleDrag = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
 
-   handleDragIn = (e) => {
-     e.preventDefault()
-     e.stopPropagation()
-     this.dragCounter += 1
-     if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-       this.setState({ dragging: true })
-     }
-   }
+  handleDragIn = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    this.dragCounter += 1
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      this.setState({ dragging: true })
+    }
+  }
 
-   handleDragOut = (e) => {
-     e.preventDefault()
-     e.stopPropagation()
-     this.dragCounter -= 1
-     if (this.dragCounter > 0) return
-     this.setState({ dragging: false })
-   }
+  handleDragOut = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    this.dragCounter -= 1
+    if (this.dragCounter > 0) return
+    this.setState({ dragging: false })
+  }
 
-   handleDrop = (e) => {
-     const {
-       handleDrop,
-     } = this.props
-     e.preventDefault()
-     e.stopPropagation()
-     this.setState({ dragging: false })
-     if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-       handleDrop(e.dataTransfer.files)
-       e.dataTransfer.clearData()
-       this.dragCounter = 0
-     }
-   }
+  handleDrop = (e) => {
+    const {
+      handleDrop,
+    } = this.props
+    e.preventDefault()
+    e.stopPropagation()
+    this.setState({ dragging: false })
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      handleDrop(e.dataTransfer.files)
+      e.dataTransfer.clearData()
+      this.dragCounter = 0
+    }
+  }
 
-   render() {
-     const {
-       title, subtitle, style, children,
-     } = this.props
-     return (
-       <div
-         ref={this.dropRef}
-       >
-         {this.state.dragging
-           && <div
-             style={style.dropDivStyle}
-           >
-             <div
-               style={style.dropTextStyle}
-             >
-               <FileUpload style={style.dropIconStyle} />
-               <div>
-                 <div style={style.dropTitleStyle}>{title}</div>
-                 <div style={style.dropSubtitleStyle}>{subtitle}</div>
-               </div>
 
-             </div>
-           </div>}
-         {children}
-       </div>
-     )
-   }
+  render() {
+    const {
+      title, subtitle, style, children,
+    } = this.props
+    return (
+      <div
+        ref={this.dropRef}
+      >
+        {this.state.dragging
+          && <div
+            style={style.dropDivStyle}
+          >
+            <div
+              style={style.dropTextStyle}
+            >
+              <FileUpload style={style.dropIconStyle} />
+              <div>
+                <div style={style.dropTitleStyle}>{title}</div>
+                <div style={style.dropSubtitleStyle}>{subtitle}</div>
+              </div>
+
+            </div>
+          </div>}
+        {children}
+      </div>
+    )
+  }
 }
 
 export default withResourceDisplayControl(DragAndDrop)
