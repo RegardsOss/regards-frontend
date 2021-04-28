f4ddb5d83879f040af2cc374caf9aa4091474f0e
lmieulet <leo.mieulet@csgroup.eu>
2021-04-28 19:43:59 +0200

    fix plugin test

--- webapp/webpack-config-front/src/app/webpack.test.config.js
+++ webapp/webpack-config-front/src/app/webpack.test.config.js
@@ -13,62 +13,12 @@ module.exports = function (projectContextPath) {
 
   return merge(config, {
     target: 'node', // in order to ignore built-in modules like path, fs, etc.
-    // externals: [
-    //   function (ctx, request, callback) {
-    //     console.log(request, callback)
-    //     if (/^yourregex$/.test(request)) {
-    //       // Externalize to a commonjs module using the request path
-    //       return callback(null, 'commonjs ' + request);
-    //     }
-
-    //     // Continue without externalizing the import
-    //     callback();
-    //   },
-    // ],
-    // externals: [nodeExternals({
-    //   whitelist: [
-    //     // this WILL include `*regardsoss*` in the bundle
-    //     /regardsoss/,
-    //     // this fix the test build dkw
-    //     /redux-api-middleware/,
-    //     // instant-mocha stuff
-    //     /chai/,
-    //     /assertion-error/,
-    //     /pathval/,
-    //     /type-detect/,
-    //     /get-func-name/,
-    //     /deep-eql/,
-    //     /check-error/,
-    //     /redux/,
-    //     /@babel/,
-    //     /react/,
-    //     /object-assign/,
-    //     /prop-types/,
-    //   ],
-    // })], // in order to ignore all modules in node_modules folder
     // Enable sourcemaps for debugging webpack's output.
     devtool: 'nosources-source-map',
     stats: {
       chunks: false,
       colors: true,
       reasons: true,
-    },
-    module: {
-      // noParse: (content) => {
-      //   const test = /iconv-loader|\/material-ui/.test(content)
-      //   if (test) {
-      //     console.log("noParse", content)
-      //     return test
-      //   }
-      //   console.log("parse", content)
-      //   return false
-      // },
-
-      // noParse: [
-      //   /sinon/,
-      //   /iconv-loader/,
-      //   /enzyme/,
-      // ],
     },
     resolve: {
       alias: {
