8350ca099d15d1680c0f8eb37ec79536dc73b2b8
lmieulet <leo.mieulet@csgroup.eu>
2021-04-29 11:08:59 +0200

    fix building app - we do not have to run lint fix on jenkins !!!

--- jenkins/node/build_webapp.sh
+++ jenkins/node/build_webapp.sh
@@ -2,8 +2,11 @@
 
 cd /app_to_build
 
+echo "Run linter"
+npm run lint --no-update-notifier || true
+
 echo "Execute tests"
-npm test --no-update-notifier
+npm run test:mocha --no-update-notifier
 
 echo "Building app"
 npm run build:production --no-update-notifier
