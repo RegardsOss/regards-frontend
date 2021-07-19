1d4e7f9bb06f6f1cd8e1e1dbe25b152d20fef141
lmieulet <leo.mieulet@csgroup.eu>
2021-07-19 11:52:02 +0200

    Bump mdi material ui version and fix registry URL

--- jenkins/node/install.sh
+++ jenkins/node/install.sh
@@ -4,7 +4,7 @@ cd /app_to_build
 
 echo "Install application dependencies and create DLL"
 # mdi-material-ui@"^4.28.0" expects material-ui@"^0.17.4 so we need to disable that check
-npm install --prefer-offline --legacy-peer-deps --no-update-notifier
+npm install --prefer-offline --no-update-notifier
 
 echo "Create production DLL"
 npm run build:production-dll --no-update-notifier
