d6887d85532c0f49acce27c1d06c32af122fbddc
TheoLasserre <theo.lasserre@csgroup.eu>
2022-03-02 17:46:49 +0100

    hotfix(167534): fix max quota & rate limit label

--- webapp/web_modules/business-modules/admin-user-projectuser-management/src/i18n/messages.en.i18n.js
+++ webapp/web_modules/business-modules/admin-user-projectuser-management/src/i18n/messages.en.i18n.js
@@ -127,10 +127,10 @@ const messages = {
   'projectUser.create.input.password.confirm': 'Confirm password (*)',
   'projectUser.create.input.status': 'Status',
   'projectUser.create.input.groups': 'Groups',
-  'projectUser.create.input.max.quota': 'Max quota',
+  'projectUser.create.input.max.quota': 'Max quota (*)',
   'projectUser.create.input.max.quota.help.message': `User max quota is the count of raw data files internally stored by REGARDS that he is allowed to download. 
   Possible values are -1, for unlimited, 0, for none, or any positive integer for a precise count`,
-  'projectUser.create.input.rate.limit': 'Rate limit',
+  'projectUser.create.input.rate.limit': 'Rate limit (*)',
   'projectUser.create.input.rate.limit.help.message': `User rate limit is the number of raw data files internally stored by REGARDS that he is allowed to download simultaneously. 
   Possible values are -1, for unlimited / 0, for none, or any positive integer for a precise count`,
   'projectUser.create.action.add': 'Add',
--- webapp/web_modules/business-modules/admin-user-projectuser-management/src/i18n/messages.fr.i18n.js
+++ webapp/web_modules/business-modules/admin-user-projectuser-management/src/i18n/messages.fr.i18n.js
@@ -127,10 +127,10 @@ const messages = {
   'projectUser.create.input.password.confirm': 'Confirmer le mot de passe (*)',
   'projectUser.create.input.status': 'Statut',
   'projectUser.create.input.groups': 'Groupes',
-  'projectUser.create.input.max.quota': 'Quota maximal',
+  'projectUser.create.input.max.quota': 'Quota maximal (*)',
   'projectUser.create.input.max.quota.help.message': `Le quota maximal d'un utilisateur désigne le nombre de fichiers de donnée brute stockés en interne par REGARDS qu'il peut télécharger. 
 Les valeurs possibles sont -1, pour illimité, 0, pour aucun, ou tout autre entier positif, pour le nombre précisément autorisé`,
-  'projectUser.create.input.rate.limit': 'Vitesse maximale de téléchargement',
+  'projectUser.create.input.rate.limit': 'Vitesse maximale de téléchargement (*)',
   'projectUser.create.input.rate.limit.help.message': `La vitesse maximale de téléchargement d'un utilisateur désigne le nombre de fichiers de donnée brute stockés en interne par REGARDS qu'il peut télécharger simultanément. 
 Les valeurs possibles sont -1, pour illimitée, 0, pour aucun, ou tout autre entier positif pour le nombre précisément autorisé`,
   'projectUser.create.action.add': 'Ajouter',
