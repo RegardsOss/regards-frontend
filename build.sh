ef1246bc80ca5df09ffc137f1515f5810a2ae9bc
Sebastien Binda <sebastien.binda@csgroup.eu>
2024-02-15 14:21:44 +0100

    fix(build): fix yarn build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf webpack-config-front/node_modules/
 rm -rf webpack-config-front/dist
 rm -rf node_modules
 find plugins -type d -name "node_modules" -exec rm -rf {} \;
-yarn --non-interactive --no-lockfile
+yarn 
 yarn build:production-dll
 echo "HACK CESIUM WEBPACK 5 - FIX CONTENT"
 sed -i "s+\".\": {+\"./\": \"./Source/\",\".\": {+g" node_modules/cesium/package.json
